a1e9208765fb17461ebc6c159a47f949dba45a85
alka077 <106584802+alka077@users.noreply.github.com>
2022-06-02 12:30:03 +0530

    Update images.ts

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -5,44 +5,46 @@ import fs, { existsSync } from 'fs';
 
 const images = express.Router();
 
-images.get(
-  '/',
-  async (req: express.Request, res: express.Response): Promise<void> => {
+images.get('/',async (req: express.Request, res: express.Response): Promise<void> => {
     const height = parseInt(req.query.height as string);
     const width = parseInt(req.query.width as string);
     const filename = req.query.filename as string;
 
-    const sourceFile = path.join(
-      __dirname,
-      '../../../images/full',
-      `/${filename}.jpg`
-    );
-    const cachePath = path.join(
-      __dirname,
-      '../../../images/resFiles',
-      `${filename}${width}&${height}.jpg`
-    );
+    const sourceFile = path.join(__dirname, '../../../images/full', `/${filename}.jpg`);
+    const cachePath = path.join(__dirname, '../../../images/resFiles', `${filename}${width}&${height}.jpg`);
 
+    //2 Invalid input for filename e.g. fjord123.
     
-    //Check if the filename has a source folder.
-
     try {
       if(fs.existsSync(sourceFile)){;
       console.log('file or directory exists');
     }else{
-        console.log('file or directory does not exist');
+        res.send('file or directory does not exist');
     }
     } catch (err) {
-      console.log('catch block error');
-        
+      console.log('catch block error');     
     }
-    //check if cached version exist or not
+    
+    //Missing filename, height or width.
+    
+    if(!filename || !width || !height){
+        res.status(400).send("please provide filename, width and height");
+    }
+    
+    
+    //Invalid input for height or width e.g. height=a, height=0 or height=-1.
+   
+    if(width <= 0 || height <= 0){
+        res.status(400).send('invalid input for height and width');
+    }
+    
+
     if (existsSync(cachePath)) {
       res.sendFile(cachePath);
     } else {
       try {
         await resizeImage(filename, width, height);
-        res.sendFile(cachePath);
+        res.status(200).sendFile(cachePath);
       } catch (err) {
         res.status(500).send('Please provide a valid image file');
       }
